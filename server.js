e1fb193166c6181c86ad9d870d274d055dcc4212
QWxwaGEgU2lsdmVyQmFjaw== <90997461+AlphaGhostUSMC@users.noreply.github.com>
2023-05-25 10:42:11 +0530

    route changed

--- server.js
+++ server.js
@@ -37,12 +37,12 @@ function verifyToken(req, res, next) {
   const token = req.cookies.token || '';
 
   if (!token) {
-    return res.send('<script>alert("You are not authorized"); window.location.href = "/";</script>');
+    return res.send('<script>alert("You are not authorized, Please Login"); window.location.href = "/login.html";</script>');
   }
 
   jwt.verify(token, secretKey, (err, decoded) => {
     if (err) {
-      return res.send('<script>alert("You are not authorized"); window.location.href = "/";</script>');
+      return res.send('<script>alert("You are not authorized, Please Login"); window.location.href = "/login.html";</script>');
     }
 
     req.decoded = decoded; // Store the decoded token in the request object
